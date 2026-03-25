1c8df70a00831d7c71b69f0a08b0a9ee3c10900c
biel19bell-droid <biel19bell@gmail.com>
2026-03-25 15:24:30 -0300

    Add files via upload

---
+++ lição3.py
@@ -0,0 +1,6 @@
+raio = int(input("Digite o raio da Caixa retangular (em cm): "))
+altura = int(input("Digite a altura da Caixa retangular (em cm): "))
+
+volume = 3.14159 * (raio ** 2) * altura
+
+print(f"Volume: {volume:.2f} cm³")
---
+++ lição4.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero: "))
+
+quadrado = numero ** 2
+
+print("O quadrado do numero é:", quadrado)
